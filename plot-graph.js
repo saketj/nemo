e7c3de3509dd41ece191b46d0bede439b1d42095
Saket Saurabh <ssaurabh@cs.wisc.edu>
2016-11-23 02:07:59 -0600

    Complete adding basic controls

--- plot-graph.js
+++ plot-graph.js
@@ -1,10 +1,35 @@
 var nodes = null;
 var edges = null;
 var network = null;
+var all_nodes = null;
+var edges = null;
+
+var indegree_param = 0;
+var outdegree_param = 0;
+var included_nodes = {};
+var included_categories = ['SSD', 'DISK', 'CPU', 'NETWORK', 'MEMORY']
 
 function draw() {
-  nodes = new vis.DataSet(dataset_nodes);
-  edges = new vis.DataSet(dataset_edges);
+  all_nodes = new vis.DataSet(dataset_nodes);
+  all_edges = new vis.DataSet(dataset_edges);
+  included_nodes = {};
+
+  var nodes = all_nodes.get({
+    filter: function(item) {
+      if ((item.indegree >= indegree_param || item.outdegree >= outdegree_param)
+            && (included_categories.indexOf(item.category)) >= 0){
+        included_nodes[item.id] = 1;
+        return 1 == 1;  // return true
+      }
+      return 1 != 1; // return false
+    }
+  });
+
+  var edges = all_edges.get({
+    filter: function(item) {
+      return (included_nodes[item.from] && included_nodes[item.to]);
+    }
+  })
 
   // Instantiate our network object.
   var container = document.getElementById('mynetwork');
@@ -15,7 +40,7 @@ function draw() {
   var options = {
     layout: {
       randomSeed : 10000,
-    },    
+    },
     nodes: {
       shape: 'dot',
       scaling: {
@@ -29,10 +54,24 @@ function draw() {
   };
   var network = new vis.Network(container, data, options);
   network.on("click", function (params) {
-    if (nodes.get(params.nodes[0]).info) {
-      document.getElementById('info').innerHTML = '<h2>Description:</h2>' + nodes.get(params.nodes[0]).info;
+    if (all_nodes.get(params.nodes[0]).info) {
+      document.getElementById('info').innerHTML = '<h2>Description:</h2>' + all_nodes.get(params.nodes[0]).info;
     } else {
       document.getElementById('info').innerHTML = '';
     }
   });
+
+  network.on("afterDrawing", function() {
+    $('#alertModal').modal('hide');
+  });
+}
+
+function applyFilter() {
+  indegree_param = $('#indegree').val() ? parseInt($('#indegree').val()) : 0;
+  outdegree_param = $('#outdegree').val() ? parseInt($('#outdegree').val()) : 0;
+  included_categories = $("input[name='category']:checked").map(function(){
+                            return $(this).val();
+                          }).get();
+  $('#alertModal').modal('show');
+  draw();
 }
--- script.py
+++ script.py
@@ -36,32 +36,61 @@ def get_year_from_citation(title):
             pub_year = year
     return pub_year
 
+def create_node_from_title(nodes, title, category):
+    title_idx = filter_title_for_index(title)
+    # Search for title in the index
+    for node_id, node_title_idx in nodes.items():
+        if match_title_idx(node_title_idx['title_idx'], title_idx):
+            return node_id
+    nodes[len(nodes)] = { 'title_idx': title_idx,
+                          'title': filter_title(title),
+                          'category': category,
+                          'year': get_year_from_citation(title),
+                          'indegree': 0,
+                          'outdegree': 0 }
+    return len(nodes) - 1
+
+
 def get_node_from_title(nodes, title):
     title_idx = filter_title_for_index(title)
-    if len(nodes) == 0:
-        nodes[0] = (title_idx, filter_title(title), get_year_from_citation(title))
-        return 0
-    else:
-        # Search for title in the index
-        for node_id, node_title_idx in nodes.items():
-            if match_title_idx(node_title_idx[0], title_idx):
-                return node_id
-        nodes[len(nodes)] = (title_idx, filter_title(title), get_year_from_citation(title))
-        return len(nodes) - 1
+    # Search for title in the index
+    for node_id, node_title_idx in nodes.items():
+        if match_title_idx(node_title_idx['title_idx'], title_idx):
+            return node_id
+    return -1
 
-def is_valid_citation(title):
+def is_valid_reference(title):
     years = re.findall("\d{4}", title)
     for year in years:
         if 1980 <= int(year) <= 2016:
             return True
     return False
 
-def generate_node_dataset(nodes, citations):
+def add_edge(nodes, edges, from_id, to_id):
+    edges[(from_id, to_id)] = 1
+    nodes[from_id]['outdegree'] = nodes[from_id]['outdegree'] + 1           # Update indegree
+    nodes[to_id]['indegree'] = nodes[to_id]['indegree'] + 1      # Update outdegree
+
+
+def generate_node_dataset(nodes):
+    # Get citations count first
+    citations = dict()
+    lines = open("citations.csv", "r").readlines()
+    for row in lines:
+        row = row.strip()
+        citation_count = int(re.compile(",").split(row, 1)[0])
+        paper_title = re.compile(",").split(row, 1)[1]
+        paper_id = get_node_from_title(nodes, paper_title)
+        if (paper_id != -1):
+            citations[paper_id] = citation_count
+
     filename = "dataset-nodes.js"
     f = open(filename, "w")
     f.write("var dataset_nodes = [\n")
     for key,value in nodes.items():
-        row = "\t{id: %d, value: %d, color: '%s', title: '%s', info: '%s'},\n" % (key, int(citations.get(key, 0)), get_color(value[2]), value[1], value[1])
+        color = get_color(value['year'], value['category'])
+        row = "\t{id: %d, value: %d, color: '%s', title: '%s', info: '%s', category: '%s', indegree: %d, outdegree: %d},\n" \
+               % (key, citations.get(key, 0), color, value['title'], value['title'], value['category'], value['indegree'], value['outdegree'])
         f.write(row)
     f.write("];");
     f.close()
@@ -79,30 +108,33 @@ def generate_edges_dataset(edges):
     return
 
 def main():
-    nodes = dict()      # dictionary of (node_id, <node_title_idx, node_title, year_of_pub>)
-    citations = dict()
+    nodes = dict()      # dictionary of (node_id, <node_title_idx, node_title, year_of_pub, indegree, outdegree>)
     edges = dict()      # list of (from, to) tuple edges
     source_dir = "references"
     for filename in os.listdir(source_dir):
         if filename.endswith(".txt"):
             filedata = open(source_dir + "/" + filename, 'r').read()
             data = re.compile("\[\d+\]").split(filedata)
-            citation_count = re.search(".*Cited by (\d+).*", data[0], re.IGNORECASE).group(1)
-            paper_title = re.compile(".*Cited by \d+\s+").split(data[0])[1]
-            paper_node_id = get_node_from_title(nodes, paper_title)
-            citations[paper_node_id] = citation_count
+            category = re.compile("\n").split(data[0])[0]       # first line in the file
+            paper_title = re.compile("\n").split(data[0])[1]    # second line in the file
+            paper_node_id = create_node_from_title(nodes, paper_title, category)
             for i in range(1,(len(data)-1)):
-                if is_valid_citation(data[i]):
-                    cited_id = get_node_from_title(nodes, data[i])
-                    edges[(cited_id, paper_node_id)] = 1
+                if is_valid_reference(data[i]):
+                    cited_id = create_node_from_title(nodes, data[i], category)
+                    add_edge(nodes, edges, cited_id, paper_node_id)
 
-    generate_node_dataset(nodes, citations)
+    generate_node_dataset(nodes)
     generate_edges_dataset(edges)
 
-def get_color(year):
-    colors = ["#21618C","#2874A6","#2E86C1","#3498DB","#5DADE2","#85C1E9","#AED6F1","#D6EAF8","#EBF5FB"]
+def get_color(year, category):
+    colors = dict()
+    colors['DISK'] = ["#FF6F00","#FF8F00","#FFA000","#FFB300","#FFC107","#FFCA28","#FFD54F","#FFE082","#FFECB3"] # amber band
+    colors['SSD'] = ["#21618C","#2874A6","#2E86C1","#3498DB","#5DADE2","#85C1E9","#AED6F1","#D6EAF8","#EBF5FB"] # blue band
+    colors['CPU'] = ["#B71C1C","#C62828","#D32F2F","#E53935","#F44336","#EF5350","#E57373","#EF9A9A","#FFCDD2"] # red band
+    colors['NETWORK'] = ["#4E342E","#5D4037","#6D4C41","#795548","#8D6E63","#A1887F","#BCAAA4","#D7CCC8"] # brown band
+    colors['MEMORY'] = ["#1B5E20","#2E7D32","#388E3C","#43A047","#4CAF50","#66BB6A","#81C784","#A5D6A7","#C8E6C9"] # gree band
     color_idx = (2016 - int(year)) / 4;  # 9 color groups
-    return colors[color_idx]
+    return colors[category][color_idx]
 
 if __name__ == "__main__":
     main()
